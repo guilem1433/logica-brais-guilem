a6ac67e08114cd525fe7e47baab7c305f0215205
gilem87 <andreovel205@gmail.com>
2025-02-13 11:49:52 -0500

    first part of exercises completed

---
+++ #00-SINTAXIS,VARIABLES,TIPOS-DE-DATOS-Y-HOLA-MUNDO.py
@@ -0,0 +1,73 @@
+""""
+
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: ¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ """
+#https://www.python.org/
+
+#1. comentario en una línea
+"""
+ comentario en varias líneas
+"""
+
+#2.Creation of a variable. As in python it is necessary to use the snake case: snake_case, all in lower
+this_is_a_variable = "esto es una variable" #a string type variable, using ""
+
+#. Python does not have variables. Anything can be call a variable
+#Eventhough in python is used a convention
+
+A_CONSTANT = "those elements writen with all upper, are to be understood as constants"
+
+#3. * - Crea variables representando todos los tipos de datos primitivos
+
+ #each language has a number of defined number of primal data.
+
+integer_1 = 876 #integrers
+
+float_1 = 43534.34454 #decimal numbers
+
+boolean_1 = True #Binary values that would only have values of true and false
+boolean_2 = False # bool / boolean
+
+string_1 = "Esta es una cadena de texto" #elements that take any element in them as text
+
+#* - Imprime por terminal el texto: ¡Hola, [y el nombre de tu lenguaje]!"
+
+print("Hola, Python")
+
+#adding somehting
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
