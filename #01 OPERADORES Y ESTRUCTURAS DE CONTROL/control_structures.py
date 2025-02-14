788163957875549ef857a1bed79ca9bad42c71a6
gilem87 <andreovel205@gmail.com>
2025-02-14 15:47:58 -0500

    in the meadle of control stuctures

---
+++ #01 OPERADORES Y ESTRUCTURAS DE CONTROL/control_structures.py
@@ -0,0 +1,24 @@
+########Control structure########333
+
+"""
+To make the code to do a certain action,
+if something takes place
+"""
+    #conditionals
+
+#if
+#taking into account the indentation of python for creating
+#function
+
+collection1 =[345,45,34] # [345,45,34,535,345345]
+
+if collection1 == [345,45,34,535,3453454]:
+    print("The numbers are right")
+    ########elseif
+    ##comparisons in the meadle or another comparison
+elif collection1 == [345,45,34]:
+    print("the number are right")
+else:
+    print("The numbers are wrong")
+
+
