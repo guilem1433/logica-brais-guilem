ee6f72e6575d9736f46554a2a6555202a46b1cd1
gilem87 <andreovel205@gmail.com>
2025-02-14 15:23:02 -0500

    belonging operators

---
+++ #01 OPERADORES Y ESTRUCTURAS DE CONTROL/belonging_operators.py
@@ -0,0 +1,18 @@
+#########Belonging operators#######3
+
+"""
+With this kind of operators,
+we are able to validate if an element is inside a collection.
+"""
+
+example1 = "G" in "Guillermo"
+print(example1)
+
+example1 = "G" not in "Guillermo"
+print(example1)
+
+example2 = [1,2,4,5] in [[1,2,4,5]]
+print(example2)
+
+example2 = [1,2,4,5] not in [[1,2,4,5]]
+print(example2)
