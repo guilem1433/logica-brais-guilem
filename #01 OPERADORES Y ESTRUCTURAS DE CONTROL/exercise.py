15fef794550c5ed61115595acb2d23f532a4e5d7
gilem87 <andreovel205@gmail.com>
2025-02-17 08:35:24 -0500

    till iterations

---
+++ #01 OPERADORES Y ESTRUCTURAS DE CONTROL/exercise.py
@@ -0,0 +1,18 @@
+
+
+# for i in range(26):
+#     i = 10
+#     i += 1
+#     print(i)
+# #anothe way
+
+for the_range in range(10,56):
+    print(the_range )
+
+####only pairs
+
+for the_range in range(10,56):
+    if the_range % 2 == 0:
+        print(the_range )
+
+
