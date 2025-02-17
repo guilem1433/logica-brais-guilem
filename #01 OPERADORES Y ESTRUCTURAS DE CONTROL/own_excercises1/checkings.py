ce99ba3d2ce64f57fb83982bb4ec74365cb61872
gilem87 <andreovel205@gmail.com>
2025-02-17 10:56:39 -0500

    making main function

---
+++ #01 OPERADORES Y ESTRUCTURAS DE CONTROL/own_excercises1/checkings.py
@@ -0,0 +1,9 @@
+#conditionals based in numbers
+
+def checkings (sum_function):
+    if sum_function < 1500:
+        print("*entrances are not matching the minimum quantities*")
+    if sum_function > 1500:
+        print("*entrances are over the expected*")
+    if sum_function == 1500:
+        print("*entrances are the expected values*")
