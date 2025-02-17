d7f03867bb45666db88363f11b0985d8e2fffd10
gilem87 <andreovel205@gmail.com>
2025-02-17 15:51:46 -0500

    making the searching function

--- #01 OPERADORES Y ESTRUCTURAS DE CONTROL/own_excercises1/searching_function.py
+++ #01 OPERADORES Y ESTRUCTURAS DE CONTROL/own_excercises1/searching_function.py
@@ -1,7 +1,24 @@
 from collector_of_values import collector_of_values
 
+def list_searched_elements():
+
+    searched_elements = []
+    while True:
+        input_searched_elements = input()
+        if input_searched_elements() == "done":
+            break
+        try:
+            searched_data = int(input_searched_elements)
+            input_searched_elements(searched_data)
+            searched_elements.append(searched_data)
+        except ValueError:
+            print("enter, valid elements")
+    return searched_elements
+
 def searching():
+
     the_number_list = collector_of_values()
+
     values_to_be_searched = input(print("enter the values to be searched"))
 
     try:
